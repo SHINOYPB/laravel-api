d838c9bde9df524b01f6a1f63a39d2055549963d
Shinoy P B <pbshinoy@gmail.com>
2023-10-31 22:07:30 +0530

    Added user factory for mass creating users.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // To run user factories use below code
+       // \App\Models\User::factory(10000)->create();
     }
 }
